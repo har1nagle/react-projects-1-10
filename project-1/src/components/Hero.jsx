2a9e73214e6dfbd96e62a76b49399643ee5d62f8
har1nagle <hnagle.0@yahoo.com>
2023-08-10 08:08:31 +0530

    my first react sample projects

---
+++ project-1/src/components/Hero.jsx
@@ -0,0 +1,38 @@
+const HeroSection = () => {
+  return (
+    <main className="hero container ">
+
+      <div className="hero-content">
+
+        <h1>
+          YOUR FEET DESERVE THE BEST
+        </h1>
+
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+        </p>
+
+        <div className="hero-btn">
+          <button>Shop Now</button>
+          <button className="secondary-btn">Category</button>
+        </div>
+
+        <div className="shopping">
+          <p>Also Available On</p>
+          <div className="brand-icon">
+            <img src="./images/flipkart.png" alt="" srcset="" />
+            <img src="./images/amazon.png" alt="" srcset="" />
+          </div>
+        </div>
+
+      </div>
+
+      <div className="hero-image">
+        <img src="./images/shoe_image.png" alt="" srcset="" />
+      </div>
+
+    </main>
+  )
+};
+
+export default HeroSection;
